a4d1a95a49d48c1f5272a99151cb38248fd02103
yonatanmk <yonatanmk@gmail.com>
2019-12-29 19:01:42 -0500

    delete user

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -24,7 +24,7 @@ router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, updateUser);
 // router.put("/user/:userId", requireSignin, hasAuthorization, updateUser);
-// router.delete("/user/:userId", requireSignin, hasAuthorization, deleteUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 // // photo
 // router.get("/user/photo/:userId", userPhoto);
 
