a878d36b3f44ad2d856bf747339f508dcca422f7
yonatanmk <yonatanmk@gmail.com>
2020-01-08 11:57:15 -0500

    documentation route

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -6,6 +6,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const expressValidator = require('express-validator');
+const fs = require('fs');
+// const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -28,6 +30,19 @@ mongoose.connect(
 mongoose.connection.on('error', err => {
   console.log(`DB connection error: ${err.message}`)
 })
+
+// apiDocs
+app.get('/', (req, res) => {
+  fs.readFile('docs/apiDocs.json', (err, data) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+      });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
 
 
 app.use(morgan('dev'));
