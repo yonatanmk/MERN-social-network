e55941daf65dc917b65d4b244402b6d0777b94a4
yonatanmk <yonatanmk@gmail.com>
2020-01-10 17:16:14 -0500

    signup action

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -5,27 +5,105 @@ import React, { Component } from "react";
 // import SocialLogin from "./SocialLogin";
 
 class Signup extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "",
-      email: "",
-      password: "",
-      error: "",
-      open: false,
-      recaptcha: false
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            error: ""
+        };
+    }
 
-  render() {
-    const { name, email, password, error, open, recaptcha } = this.state;
-    return (
-      <div className="container">
-        <h2 className="mt-5 mb-5">Signup</h2>
-      </div>
-    );
-  }
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    clickSubmit = event => {
+        event.preventDefault();
+        const { name, email, password } = this.state;
+        const user = {
+            name,
+            email,
+            password
+        };
+        this.signUp(user);
+    };
+
+    signUp = user => {
+        return fetch("http://localhost:8080/signup", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(response => {
+                console.log(response)
+                return response.json();
+            })
+            .then(data => {
+                if (data.error) {
+                    this.setState({ error: data.error });
+                } else {
+                    this.setState({
+                        name: "",
+                        email: "",
+                        password: "",
+                        error: ""
+                    });
+                }
+            })
+            .catch(err => console.error(err));
+    }
+
+    render() {
+        const { name, email, password } = this.state;
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Signup</h2>
+
+                <form>
+                    <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input
+                            onChange={this.handleChange("name")}
+                            type="text"
+                            className="form-control"
+                            value={name}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input
+                            onChange={this.handleChange("email")}
+                            type="email"
+                            className="form-control"
+                            value={email}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Password</label>
+                        <input
+                            onChange={this.handleChange("password")}
+                            type="password"
+                            className="form-control"
+                            value={password}
+                        />
+                    </div>
+                    <button
+                        onClick={this.clickSubmit}
+                        className="btn btn-raised btn-primary"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
+
 
 export default Signup;
 
